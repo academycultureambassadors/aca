ed167ce4c601bc49c6b55460577518e996c5309e
Axel Valdez <axel@paskola.com>
2018-10-31 23:54:46 -0700

    Add omaha chapter

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ function animateHeaderBlocks() {
 }
 
 headerAnimation = setInterval(animateHeaderBlocks, 1500);
+
+//OVERLAYS
+
+var overlays = document.querySelectorAll(".overlay");
+var closeLinks = document.querySelectorAll(".close");
+var openLinks = document.querySelectorAll(".open-overlay");
+var bodyElement = document.querySelector("body");
+
+closeLinks.forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    overlays.forEach(overlay => {
+      overlay.classList.remove("open");
+    });
+    bodyElement.style.overflowY = "auto";
+  });
+});
+
+openLinks.forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("opens")).classList.add("open");
+    bodyElement.style.overflowY = "hidden";
+  });
+});
